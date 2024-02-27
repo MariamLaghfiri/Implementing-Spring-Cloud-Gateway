8f527b3a6fd7f2004fa159f3d48783c3c01c611d
Yassin Ezaouibi <zouibiyassin03@gmail.com>
2024-02-28 00:25:03 +0100

    the project is updated

--- gateway-service-config/src/main/java/org/example/gatewayserviceconfig/config/SpringCloudConfig.java
+++ gateway-service-config/src/main/java/org/example/gatewayserviceconfig/config/SpringCloudConfig.java
@@ -1,8 +1,11 @@
 package org.example.gatewayserviceconfig.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.cloud.gateway.server.mvc.filter.
+
+/*
 @Configuration
 public class SpringCloudConfig {
 
@@ -15,7 +18,39 @@ public class SpringCloudConfig {
 
                 .route(r -> r.path("/consumer/**")
                         .uri("http://localhost:8082/")
-                        .id("consumerModule"))
+                        .("consumerModule"))
                 .build();
     }
+}
+*/
+
+@Configuration
+public class SpringCloudConfig {
+
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/employee/**")
+                        .uri("http://localhost:8081/")
+                        /*.id("employeeModule")*/)
+
+                .route(r -> r.path("/consumer/**")
+                        .uri("http://localhost:8082/")
+                        /*.id("consumerModule")*/)
+                .build();
+    }
+
+  /*  @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/employee/**")
+                        .uri("http://localhost:8081/")
+                        .id("employeeModule"))
+
+                .route(r -> r.path("/consumer/**")
+                        .uri("http://localhost:8082/")
+                        .id("consumerModule"))
+                .build();
+    }*/
 }
